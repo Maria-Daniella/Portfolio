a2b1d76440123f9d10e0488c868a8053a5a885b0
Maria-Daniella <41889258+Maria-Daniella@users.noreply.github.com>
2022-05-12 13:26:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Front-End Developer", "Back-End Developer"],
+        strings: ["Web Developer", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Front End-Developer", "Back-End Developer"],
+        strings: ["Web Developer", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
